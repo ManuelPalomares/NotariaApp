96571761e0ff8ca30eea9df16de617d55e0d5400
ManuelPalomares <mpalomar@MacBook-Pro-de-Manuel.local>
2015-09-28 21:32:23 -0500

    Ejemplo menu

--- app/Application.js
+++ app/Application.js
@@ -1,65 +1,61 @@
 Ext.define('NotariaApp.Application', {
     extend: 'Ext.app.Application',
-    
     name: 'NotariaApp',
-
     stores: [
         'NotariaApp.store.MenuStore'
     ],
-    
-    views : ['NotariaApp.view.login.LoginForm',
+    views: [
+        'NotariaApp.view.login.LoginForm',
         'NotariaApp.view.formularios.FormularioPrueba',
         'NotariaApp.view.main.Menus',
         'NotariaApp.view.main.TabContenedorPpal',
-        'NotariaApp.view.formularios.FormularioFactura'
+        'NotariaApp.view.formularios.FormularioFactura',
+        'NotariaApp.view.reportes.ReporteFacturas'
     ],
-   
     controllers: ['NotariaApp.controller.HostServer'],
-    
     launch: function () {
         // TODO - Launch the application
-       
-  
+
+
         var host = Ext.create("NotariaApp.controller.HostServer").getHost();
         Ext.Ajax.request({
-            url: host+"php/session/session.php",
-            success: function(response){
+            url: host + "php/session/session.php",
+            success: function (response) {
                 var result = Ext.decode(response.responseText);
                 //valida si el usuario no esta registrado
-                if(result.message_error === "Usuario no registrado"){
-                    var win = Ext.create("Ext.window.Window",{
-                        width : 300,
-                        id : "x_ventanalogin",
-                        height : 200,
-                        modal  : true,
-                        title : "Inicio de sesion",
+                if (result.message_error === "Usuario no registrado") {
+                    var win = Ext.create("Ext.window.Window", {
+                        width: 300,
+                        id: "x_ventanalogin",
+                        height: 200,
+                        modal: true,
+                        title: "Inicio de sesion",
                         resizable: false,
                         closable: false,
-                        items : [{xtype  : "x_loginForm"}]
-               
+                        items: [{xtype: "x_loginForm"}]
+
                     });
                     win.show();
                     return;
                 }
 
-                 Ext.create("NotariaApp.view.main.Viewport");
-                
-                Ext.getCmp("x_labelNombreUsuario").setText("Usuario : "+result.nombre);
+                Ext.create("NotariaApp.view.main.Viewport");
+
+                Ext.getCmp("x_labelNombreUsuario").setText("Usuario : " + result.nombre);
             },
-            failure: function(response){
+            failure: function (response) {
                 alert('Error realizando su solicitud por favor comunicar con su administrador :' + response.status);
             }
         });
-        
-    },
 
+    },
     onAppUpdate: function () {
         Ext.Msg.confirm('Application Update', 'This application has an update, reload?',
-            function (choice) {
-                if (choice === 'yes') {
-                    window.location.reload();
+                function (choice) {
+                    if (choice === 'yes') {
+                        window.location.reload();
+                    }
                 }
-            }
         );
     }
 });
---
+++ app/view/reportes/ReporteFacturas.js
@@ -0,0 +1,5 @@
+Ext.define('NotariaApp.view.reportes.ReporteFacturas', {
+    extend: 'Ext.form.Panel',
+    alias: 'widget.x_reporte_facturas',
+    html: 'Prueba Reportes'
+});
